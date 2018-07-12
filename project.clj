e30078ee38c3bff8023a8944ad479902acfed952
denis6705 <denis6705@gmail.com>
2018-07-12 16:24:47 +0300

    sqlite example code

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+				 [org.clojure/java.jdbc "0.7.1"]
+				 [org.xerial/sqlite-jdbc "3.23.1"]]
   :main ^:skip-aot bla.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
