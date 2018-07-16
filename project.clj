bc4537f038834971d3ad3e29f41ca74156cd1cd9
denis6705 <denis6705@gmail.com>
2018-07-16 11:55:16 +0300

    add some librarys to project.clj, and startserver

--- project.clj
+++ project.clj
@@ -4,9 +4,14 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-				 [org.clojure/java.jdbc "0.7.1"]
-				 [org.xerial/sqlite-jdbc "3.23.1"]
-         [org.clojars.pntblnk/clj-ldap "0.0.16"]]
+                 [org.clojure/java.jdbc "0.7.1"]
+                 [org.xerial/sqlite-jdbc "3.23.1"]
+                 [org.clojars.pntblnk/clj-ldap "0.0.16"]
+                 [ring "1.6.3"]
+                 [ring/ring-defaults "0.3.1"]
+                 [compojure "1.6.1"]
+                 [http-kit "2.3.0"]
+                 [hiccup "1.0.5"]]
   :main ^:skip-aot bla.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/bla/core.clj
+++ src/bla/core.clj
@@ -1,10 +1,16 @@
 (ns bla.core
   (:gen-class)
-  (:require [bla.ldap-auth :as auth]))
+  (:require [bla.ldap-auth :as auth]
+            [org.httpkit.server :refer :all]
+            [compojure.core :refer :all]
+            [compojure.route :as route]))
 
-
+(defroutes app
+  (GET "/" [] "<h1>Hello World</h1>")
+  (route/not-found "<h1>Page not found</h1>"))
 
 
 
 (defn -main
-  [])
+  []
+  (run-server #'app {:port 80}))
