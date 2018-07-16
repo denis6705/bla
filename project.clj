56e04a823c29c4b4ff54e5bf5c1ff3354a35ab49
Denis Shkabatur <denis605@gmail.com>
2018-07-16 23:02:09 +0300

    some home changes

--- project.clj
+++ project.clj
@@ -9,9 +9,12 @@
                  [org.clojars.pntblnk/clj-ldap "0.0.16"]
                  [ring "1.6.3"]
                  [ring/ring-defaults "0.3.1"]
+                 [com.cemerick/friend "0.2.3"]
+                 [ring/ring-ssl "0.3.0"]
                  [compojure "1.6.1"]
                  [http-kit "2.3.0"]
                  [hiccup "1.0.5"]]
+
   :main ^:skip-aot bla.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/bla/core.clj
+++ src/bla/core.clj
@@ -1,12 +1,21 @@
 (ns bla.core
   (:gen-class)
-  (:require [bla.ldap-auth :as auth]
+  (:require ;[bla.ldap-auth :as auth]
             [org.httpkit.server :refer :all]
             [compojure.core :refer :all]
             [compojure.route :as route]
             [hiccup.core :as hc]
-            [hiccup.page :as hp]))
+            [hiccup.page :as hp]
+            [ring.middleware.defaults :refer :all]
+            [ring.util.response :refer :all]
+            [ring.middleware.ssl :refer [wrap-hsts]]
+            [cemerick.friend :as friend]
+            (cemerick.friend [workflows :as workflows]
+                             [credentials :as creds])))
 
+(defn authenticated?
+  [map {name :user-name pass :password}]
+  (print pass name))
 
 (defn index [req]
   (hp/html5
@@ -15,14 +24,18 @@
     [:body
      [:div {:class "containder"}
        [:h1 "WHF"]
-       [:h1 (str req)]]]))
+       [:h2 (str req)]]]))
+
 
 (defroutes app
   (GET "/" [] index)
   (route/not-found "<h1>Page not found</h1>"))
 
-
+(def site
+ (-> app
+  wrap-hsts))
 
 (defn -main
   []
-  (run-server #'app {:port 80}))
+  (run-server #'site {:port 80})
+  (println "server started at localhost:80"))
